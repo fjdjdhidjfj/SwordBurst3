b33759c09a279db226686419861dc984c888cc81
fjdjdhidjfj <126058446+fjdjdhidjfj@users.noreply.github.com>
2024-05-26 23:49:10 +0500

    Update spawns.lua

--- spawns.lua
+++ spawns.lua
@@ -1,17 +1,24 @@
 Spawns = {
-	[1] = {
-		["Razor Boar"] = CFrame.new(840, 133, -700),
-		["Tortoise"] = CFrame.new(1300, 130, -451),
-		["Wolf"] = CFrame.new(915, 132, 250),
-		["Brown Bear"] = CFrame.new(815, 151, 1025),
-		["Rock Golem"] = CFrame.new(215, 211, 1400),
-		["Crystal Boar"] = CFrame.new(-190, 152, 720),
-		["Thunder Sakura Moose"] = CFrame.new(-740, 209, -58),
-		["Soldier Boar"] = CFrame.new(-1510, -43, 1335),
-		["Basilisk"] = CFrame.new(-1050, -73, 1225)
-	},
-	[2] = {
-		--[""] = CFrame.new()
-	}	
+    [1] = {
+        ["Razor Boar"] = CFrame.new(840, 133, -700),
+        ["Tortoise"] = CFrame.new(1300, 130, -451),
+        ["Wolf"] = CFrame.new(915, 132, 250),
+        ["Brown Bear"] = CFrame.new(815, 151, 1025),
+        ["Rock Golem"] = CFrame.new(215, 211, 1400),
+        ["Crystal Boar"] = CFrame.new(-190, 152, 720),
+        ["Thunder Sakura Moose"] = CFrame.new(-740, 209, -58),
+        ["Soldier Boar"] = CFrame.new(-1510, -43, 1335),
+        ["Basilisk"] = CFrame.new(-1050, -73, 1225)
+    },
+    [2] = {
+        ["Fire Wasp"] = CFrame.new(15, 363, 1240),
+        ["Ember Jaguar"] = CFrame.new(905, 406, 105),
+        ["Lava Basilisk"] = CFrame.new(1310, 406, -365),
+        ["Skeleton Bear"] = CFrame.new(560, 405, -945),
+        ["Fiery Moose"] = CFrame.new(-90, 385, -2235),
+        ["Magma Golem"] = CFrame.new(-1460, 402, -2520),
+        ["Phoenix"] = CFrame.new(-1660, 511, -1020)
+        --[""] = CFrame.new(),
+    }	
 }
 return Spawns
