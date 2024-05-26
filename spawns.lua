ebbef0354a01e264d53ddd46aaf111dbe87b19de
fjdjdhidjfj <126058446+fjdjdhidjfj@users.noreply.github.com>
2024-05-26 22:18:07 +0500

    Update spawns.lua

--- spawns.lua
+++ spawns.lua
@@ -6,9 +6,12 @@ Spawns = {
         ["Brown Bear"] = CFrame.new(815, 151, 1025),
         ["Rock Golem"] = CFrame.new(215, 211, 1400),
         ["Crystal Boar"] = CFrame.new(-190, 152, 720),
-        ["Thunder Sakura Moose"] = CFrame.new(-740, 209, -58)
+        ["Thunder Sakura Moose"] = CFrame.new(-740, 209, -58),
         ["Soldier Boar"] = CFrame.new(-1510, -43, 1335),
         ["Basilisk"] = CFrame.new(-1050, -73, 1225)
+    },
+    [2] = {
+        --[""] = CFrame.new()
     }
 }
 return Spawns
