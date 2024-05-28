4cd7730100d46f9797d9fa5ddfce2ab6acf1c216
fjdjdhidjfj <126058446+fjdjdhidjfj@users.noreply.github.com>
2024-05-29 02:47:21 +0500

    Update bosses.lua

--- bosses.lua
+++ bosses.lua
@@ -1,23 +1,7 @@
 bosses = {
-    [1] = {
-        ["Hagan"] = 10,
-        ["Galiard"] = 20,
-        ["Elron The Frenzy Lord"] = 25
-    },
-    [2] = {
-        ["Roaring Ogre"] = 30,
-        ["Erebus"] = 40,
-        ["Pathos The Fire Lord"] = 45
-    },
-    [3] = {
-        ["Alpha Icewhal"] = 50,
-        ["Yeti"] = 60,
-        ["Elize The Siren"] = 65,
-    },
-    [4] = {
-        ["Desert Spirit"] = 70,
-        ["Sand Terror"] = 80,
-        ["Nestor The Guardian"] = 85
-    }
+    [1] = {"Hagan", "Galiard", "Elron The Frenzy Lord"},
+    [2] = {"Roaring Ogre", "Erebus", "Pathos The Fire Lord"},
+    [3] = {"Alpha Icewhal", "Yeti", "Elize The Siren"},
+    [4] = {"Desert Spirit", "Sand Terror", "Nestor The Guardian"}
 }
 return bosses
