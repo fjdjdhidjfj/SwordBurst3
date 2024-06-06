4b04119d424fd53f189e32389cf34cae82ba88b0
fjdjdhidjfj <126058446+fjdjdhidjfj@users.noreply.github.com>
2024-06-07 02:45:57 +0500

    Update mobs.lua

--- mobs.lua
+++ mobs.lua
@@ -1,5 +1,5 @@
 mobs = {
-    [1] = {"Razor Boar", "Tortoise", "Wolf", "Brown Bear", "Rock Golem", "Crystal Boar", "Thunder Sakura Moose", "Soldier Boar", "Basilisk"},
+    [1] = {"Razor Boar", "Tortoise", "Wolf", "Brown Bear", "Rock Golem", "Crystal Boar", "Thunder Sakura Moose", "Jackalope", "Soldier Boar", "Basilisk"},
     [2] = { "Fire Wasp", "Ember Jaguar", "Lava Basilisk", "Skeleton Bear", "Fiery Moose", "Magma Golem", "Phoenix", "Fire Imp", "Hell Hound"},
     [3] = {"Mist Bunny", "Penguin", "Frost Scorpion", "Snow Kitsune", "Chill Bat", "Polar Bear", "Cold Mammoth", "Icewhal", "Ice Wraith"},
     [4] = {"Crocodile", "Raptor", "Desert Phoenix", "Spirit Lion", "Oasis Tortoise", "Dragonfly", "Camel", "Tomb Scorpion", "Jeweled Scarab"}
