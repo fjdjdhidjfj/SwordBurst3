91931e5c4e915daf21d4bfe8c822c51336ef663c
fjdjdhidjfj <126058446+fjdjdhidjfj@users.noreply.github.com>
2024-06-07 21:06:57 +0500

    Update mobs.lua

--- mobs.lua
+++ mobs.lua
@@ -1,7 +1,13 @@
 mobs = {
     [1] = {"Razor Boar", "Tortoise", "Wolf", "Brown Bear", "Rock Golem", "Crystal Boar", "Thunder Sakura Moose", "Jackalope", "Soldier Boar", "Basilisk"},
-    [2] = { "Fire Wasp", "Ember Jaguar", "Lava Basilisk", "Skeleton Bear", "Fiery Moose", "Magma Golem", "Phoenix", "Fire Imp", "Hell Hound"},
+    [2] = {"Fire Wasp", "Ember Jaguar", "Lava Basilisk", "Skeleton Bear", "Fiery Moose", "Magma Golem", "Phoenix", "Fire Imp", "Hell Hound"},
     [3] = {"Mist Bunny", "Penguin", "Frost Scorpion", "Snow Kitsune", "Chill Bat", "Polar Bear", "Cold Mammoth", "Icewhal", "Ice Wraith"},
-    [4] = {"Crocodile", "Raptor", "Desert Phoenix", "Spirit Lion", "Oasis Tortoise", "Dragonfly", "Camel", "Tomb Scorpion", "Jeweled Scarab"}
+    [4] = {"Crocodile", "Raptor", "Desert Phoenix", "Spirit Lion", "Oasis Tortoise", "Dragonfly", "Camel", "Tomb Scorpion", "Jeweled Scarab"},
+    ["Tower"] = {
+        "Razor Boar", "Tortoise", "Wolf", "Brown Bear", "Rock Golem", "Crystal Boar", "Thunder Sakura Moose", "Jackalope", "Soldier Boar", "Basilisk",
+        "Fire Wasp", "Ember Jaguar", "Lava Basilisk", "Skeleton Bear", "Fiery Moose", "Magma Golem", "Phoenix", "Fire Imp", "Hell Hound",
+        "Mist Bunny", "Penguin", "Frost Scorpion", "Snow Kitsune", "Chill Bat", "Polar Bear", "Cold Mammoth", "Icewhal", "Ice Wraith",
+        "Crocodile", "Raptor", "Desert Phoenix", "Spirit Lion", "Oasis Tortoise", "Dragonfly", "Camel", "Tomb Scorpion", "Jeweled Scarab"
+    }
 }
 return mobs
