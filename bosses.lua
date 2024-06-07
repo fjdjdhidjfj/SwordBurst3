88ed0b47d89c73da9e1904dfb22123e447c5edbf
fjdjdhidjfj <126058446+fjdjdhidjfj@users.noreply.github.com>
2024-06-07 21:06:01 +0500

    Update bosses.lua

--- bosses.lua
+++ bosses.lua
@@ -2,6 +2,7 @@ bosses = {
     [1] = {"Hagan", "Galiard", "Elron The Frenzy Lord"},
     [2] = {"Roaring Ogre", "Erebus", "Pathos The Fire Lord"},
     [3] = {"Alpha Icewhal", "Yeti", "Elize The Siren"},
-    [4] = {"Desert Spirit", "Sand Terror", "Nestor The Guardian"}
+    [4] = {"Desert Spirit", "Sand Terror", "Nestor The Guardian"},
+    ["Tower"] = {"Hagan", "Galiard", "Elron The Frenzy Lord", "Roaring Ogre", "Erebus", "Pathos The Fire Lord", "Alpha Icewhal", "Yeti", "Elize The Siren", "Desert Spirit", "Sand Terror", "Nestor The Guardian"}
 }
 return bosses
