c961c8c173e95812b80e32909088d4aec8743890
fjdjdhidjfj <126058446+fjdjdhidjfj@users.noreply.github.com>
2025-11-16 23:47:00 +0500

    Update spawns.lua

--- spawns.lua
+++ spawns.lua
@@ -1,48 +1,44 @@
 local spawns = {
-    [1] = {
-        ["Razor Boar"] = CFrame.new(840, 103, -700),
-        ["Tortoise"] = CFrame.new(1300, 100, -451),
-        ["Wolf"] = CFrame.new(915, 132, 220),
-        ["Brown Bear"] = CFrame.new(815, 121, 1025),
-        ["Rock Golem"] = CFrame.new(215, 181, 1400),
-        ["Crystal Boar"] = CFrame.new(-190, 122, 720),
-        ["Thunder Sakura Moose"] = CFrame.new(-740, 179, -58),
-        ["Jackalope"] = CFrame.new(-1455, 217, -45),
-        ["Soldier Boar"] = CFrame.new(-1510, -73, 1335),
-        ["Basilisk"] = CFrame.new(-1050, -103, 1225)
-    },
-    [2] = {
-        ["Fire Wasp"] = CFrame.new(15, 333, 1240),
-        ["Ember Jaguar"] = CFrame.new(905, 376, 105),
-        ["Lava Basilisk"] = CFrame.new(1310, 376, -365),
-        ["Skeleton Bear"] = CFrame.new(560, 375, -945),
-        ["Fiery Moose"] = CFrame.new(-90, 355, -2235),
-        ["Magma Golem"] = CFrame.new(-1460, 372, -2520),
-        ["Phoenix"] = CFrame.new(-1660, 481, -1020),
-        ["Fire Imp"] = CFrame.new(-745, -282, -3520),
-        ["Hell Hound"] = CFrame.new(-395, -302, -1470)
-    },
-    [3] = {
-        ["Mist Bunny"] = CFrame.new(-160, -12, 2005),
-        ["Penguin"] = CFrame.new(130, -19, 820),
-        ["Frost Scorpion"] = CFrame.new(120, 28, -310),
-        ["Snow Kitsune"] = CFrame.new(-400, 55, -1785),
-        ["Chill Bat"] = CFrame.new(-1540, 185, -1990),
-        ["Polar Bear"] = CFrame.new(-2710, 202, -690),
-        ["Cold Mammoth"] = CFrame.new(-3005, 205, 155),
-        ["Icewhal"] = CFrame.new(-23585, 2928, 410),
-        ["Ice Wraith"] = CFrame.new(-22945, 2942, 1070)
-    },
-    [4] = {
-        ["Crocodile"] = CFrame.new(485, 122, -1690),
-        ["Raptor"] = CFrame.new(-620, 123, -835),
-        ["Desert Phoenix"] = CFrame.new(-1150, 123, -690),
-        ["Spirit Lion"] = CFrame.new(-665, 92, 645),
-        ["Oasis Tortoise"] = CFrame.new(1260, 124, 75),
-        ["Dragonfly"] = CFrame.new(2135, 125, 1005),
-        ["Camel"] = CFrame.new(970, 123, 1775),
-        ["Tomb Scorpion"] = CFrame.new(235, -384, 3405),
-        ["Jeweled Scarab"] = CFrame.new(420, -384, 4250)
-    }
+    --1
+    ["Razor Boar"] = CFrame.new(840, 103, -700),
+    ["Tortoise"] = CFrame.new(1300, 100, -451),
+    ["Wolf"] = CFrame.new(915, 132, 220),https://github.com/fjdjdhidjfj/SwordBurst3/blob/main/spawns.lua
+    ["Brown Bear"] = CFrame.new(815, 121, 1025),
+    ["Rock Golem"] = CFrame.new(215, 181, 1400),
+    ["Crystal Boar"] = CFrame.new(-190, 122, 720),
+    ["Thunder Sakura Moose"] = CFrame.new(-740, 179, -58),
+    ["Jackalope"] = CFrame.new(-1455, 217, -45),
+    ["Soldier Boar"] = CFrame.new(-1510, -73, 1335),
+    ["Basilisk"] = CFrame.new(-1050, -103, 1225),
+    --2
+    ["Fire Wasp"] = CFrame.new(15, 333, 1240),
+    ["Ember Jaguar"] = CFrame.new(905, 376, 105),
+    ["Lava Basilisk"] = CFrame.new(1310, 376, -365),
+    ["Skeleton Bear"] = CFrame.new(560, 375, -945),
+    ["Fiery Moose"] = CFrame.new(-90, 355, -2235),
+    ["Magma Golem"] = CFrame.new(-1460, 372, -2520),
+    ["Phoenix"] = CFrame.new(-1660, 481, -1020),
+    ["Fire Imp"] = CFrame.new(-745, -282, -3520),
+    ["Hell Hound"] = CFrame.new(-395, -302, -1470),
+    --3
+    ["Mist Bunny"] = CFrame.new(-160, -12, 2005),
+    ["Penguin"] = CFrame.new(130, -19, 820),
+    ["Frost Scorpion"] = CFrame.new(120, 28, -310),
+    ["Snow Kitsune"] = CFrame.new(-400, 55, -1785),
+    ["Chill Bat"] = CFrame.new(-1540, 185, -1990),
+    ["Polar Bear"] = CFrame.new(-2710, 202, -690),
+    ["Cold Mammoth"] = CFrame.new(-3005, 205, 155),
+    ["Icewhal"] = CFrame.new(-23585, 2928, 410),
+    ["Ice Wraith"] = CFrame.new(-22945, 2942, 1070),
+    --4
+    ["Crocodile"] = CFrame.new(485, 122, -1690),
+    ["Raptor"] = CFrame.new(-620, 123, -835),
+    ["Desert Phoenix"] = CFrame.new(-1150, 123, -690),
+    ["Spirit Lion"] = CFrame.new(-665, 92, 645),
+    ["Oasis Tortoise"] = CFrame.new(1260, 124, 75),
+    ["Dragonfly"] = CFrame.new(2135, 125, 1005),
+    ["Camel"] = CFrame.new(970, 123, 1775),
+    ["Tomb Scorpion"] = CFrame.new(235, -384, 3405),
+    ["Jeweled Scarab"] = CFrame.new(420, -384, 4250)
 }
 return spawns
