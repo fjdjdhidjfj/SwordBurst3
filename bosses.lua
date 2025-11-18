0a576caf99f4673e6fd447ee5534472b3fde0959
fjdjdhidjfj <126058446+fjdjdhidjfj@users.noreply.github.com>
2025-11-18 15:30:50 +0500

    Update bosses.lua

--- bosses.lua
+++ bosses.lua
@@ -3,6 +3,9 @@ local bosses = {
     [2] = {"Roaring Ogre", "Erebus", "Pathos The Fire Lord"},
     [3] = {"Alpha Icewhal", "Yeti", "Elize The Siren"},
     [4] = {"Desert Spirit", "Sand Terror", "Nestor The Guardian"},
+    [5] = {},
+    [6] = {},
+    [7] = {"Callisto", "Umbra", "Altalune"},
     ["Tower"] = {
         "Hagan", "Galiard", "Elron The Frenzy Lord",
         "Roaring Ogre", "Erebus", "Pathos The Fire Lord",
